3345ef7654ab287e0795a30102def501baf99c49
dbabichenko <dmb72@pitt.edu>
2026-04-20 18:41:52 -0400

    Update app.py

--- app.py
+++ app.py
@@ -157,5 +157,5 @@ def delete_annotation(ann_id):
     return jsonify({'deleted': ann_id})
 
 if __name__ == '__main__':
-    # app.run(debug=True)
-    app.run(debug=False, host='0.0.0.0', port=80)
+    app.run(debug=True)
+    # app.run(debug=False, host='0.0.0.0', port=80)
